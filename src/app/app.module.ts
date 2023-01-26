8756d0b0d6ebc14a0566bee3bbd18edf8eeea080
Gustavo Pereira <you@example.com>
2023-01-26 00:46:53 -0300

    Melhorias no fluxo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CadastroUsuarioComponent } from './pages/cadastro-usuario/cadastro-usuario.component';
 import { CampanhasComponent } from './pages/campanhas/campanhas.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ListaCampanhasComponent } from './pages/lista-campanhas/lista-campanhas.component';
 registerLocaleData(ptBr);
 
 @NgModule({
@@ -63,6 +64,7 @@ registerLocaleData(ptBr);
     ListaLojasComponent,
     CadastroUsuarioComponent,
     CampanhasComponent,
+    ListaCampanhasComponent,
     
   ],
   imports: [
